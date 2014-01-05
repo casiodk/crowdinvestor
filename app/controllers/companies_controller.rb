fd38a543e4aeecbfdbc2a5f37594a904ee71f526
Nicolai Seerup <mail@soundtracktor.com>
2014-01-05 20:56:34 +0100

    h

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,6 @@
 class CompaniesController < ApplicationController
 	def show
-		@executive_summary_points = ["75% funded", "1.5 mio Dkk investeret", "2 mio Dkk målsætning", "21 investorer", "23 dage tilbage", "Cap value 5.8 mio Dkk" ]
+		@executive_summary_points = ["75% funded", "1.5 mio Dkk investeret", "2 mio Dkk målsætning", "21 investorer", "23 dage tilbage", "Valuering 5.8 mio Dkk" ]
 		@company_name = "Soundtracktor APS"
 	end
 end
