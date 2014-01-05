fee30409139027bfefd3f7cce6b034e62181f46b
Nicolai Seerup <mail@soundtracktor.com>
2014-01-05 21:01:43 +0100

    fix company name

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,7 @@
 class CompaniesController < ApplicationController
 	def show
 		@executive_summary_points = ["75% funded", "1.5 mio Dkk investeret", "2 mio Dkk målsætning", "21 investorer", "23 dage tilbage", "Valuering 5.8 mio Dkk" ]
-		@company_name = "Soundtracktor APS"
+		@company_name = "Soundtracktor ApS"
 	end
 end
 
